58eb760b6700eddc49473b9583aca36795705a2b
sknpp_ftr <sknpp_ftr@63df29d3-4323-0410-adbf-b7f0d5065d14>
2007-03-02 12:57:33 +0000

    add some sweetness
    git-svn-id: http://sknpp.unfuddle.com/svn/sknpp_sknpp/Package-Subroutine@22 63df29d3-4323-0410-adbf-b7f0d5065d14

---
+++ lib/Package/Subroutine/Sugar.pm
@@ -0,0 +1,18 @@
+  package Package::Subroutine::Sugar
+# **********************************
+; our $VERSION='0.01'
+
+; package from
+# ************
+; our $VERSION='0.07'
+; use Package::Subroutine 'Package::Subroutine' => qw/import export version/ 
+
+; package code
+# ************
+; our $VERSION='0.01'
+; use Package::Subroutine 'Package::Subroutine' => qw/install/
+
+; 1
+
+__END__
+
