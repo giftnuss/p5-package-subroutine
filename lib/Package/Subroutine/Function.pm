f620403a151c1230e4b84167a2b31e0eced86d4f
sknpp_ftr <sknpp_ftr@63df29d3-4323-0410-adbf-b7f0d5065d14>
2008-02-06 01:58:56 +0000

    einen preis für gute namen werde ich wohl nicht kriegen
    git-svn-id: http://sknpp.unfuddle.com/svn/sknpp_sknpp/Package-Subroutine@80 63df29d3-4323-0410-adbf-b7f0d5065d14

--- lib/Package/Subroutine/Function.pm
+++ lib/Package/Subroutine/Function.pm
@@ -28,10 +28,10 @@
         }
     ; return $ref
     }
-    
+
 ; sub setter
     { my ($obj,$method,@args) = @_
-    ; if($args[0])
+    ; if(@args)
         {
             return $obj->$method(@args)
         }
@@ -57,5 +57,32 @@ Package::Subroutine::Function
     perform(undef); # returns undef
     perform($not_a_code_ref); # returns $not_a_code_ref
     perform($coderef,@args);  # same as with run
+
+    # calls $method on $obj only when @args is not ().
+    setter($obj,$method,@args);
     
 =head1 DESCRIPTION
+
+These are all tiny helper functions provided by this package.
+
+C<setter> returns undef when there are no arguments.
+
+=head1 AUTHOR
+
+Sebastian Knapp, <rock@ccls-online.de>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-package-subroutine@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyleft 2007-2008 Sebastian Knapp
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
