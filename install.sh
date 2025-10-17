0290da874a3de043bc030042913923c389ca3f95
Polis Subdance <treepolis@ecobytes.net>
2025-10-17 10:13:48 +0200

    use minicpan if available

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ __DIR__=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 cd $__DIR__
 
-mkdir -p vendor
+mkdir -p vendor   
 
-cpanm -L vendor --self-contained <requirements.txt
-cpanm -L vendor --self-contained <requirements-dev.txt
+if [ -n "$MINICPAN" ] ; then
+    FROM="--from $MINICPAN"
+else
+    FROM=""
+fi
+
+cpanm -L vendor $FROM --self-contained <requirements.txt
+cpanm -L vendor $FROM --self-contained <requirements-dev.txt
